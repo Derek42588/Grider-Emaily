f6e609483b8fac58687331c5c75359cf5488e810
Derek Pyle <derekpyle@gmail.com>
2020-04-09 11:38:52 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'})
+})
+
+app.get('/greeting', (req, res) => {
+    res.send({denise: 'butt'})
+}) 
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT)
