86cb0e42c9370bb98d1106245ec4fad4a46c56f0
Derek Pyle <derekpyle@gmail.com>
2020-04-09 12:03:24 -0400

    changed greeting

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'})
-})
+  res.send({ bye: 'buddy ' });
+});
 
 app.get('/greeting', (req, res) => {
-    res.send({denise: 'butt'})
-}) 
+  res.send({ denise: 'butt' });
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT)
+app.listen(PORT);
