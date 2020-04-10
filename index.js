927d20631752810ba7c4e90dfd1c4e290526c0cf
Derek Pyle <derekpyle@gmail.com>
2020-04-10 10:56:19 -0400

    first prod, finished auth flow

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+require('./models/User');
+require('./services/passport');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy ' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
-app.get('/greeting', (req, res) => {
-  res.send({ denise: 'butt' });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
