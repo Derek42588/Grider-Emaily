384c254297645a4675ad5b32e6b35cdb691e40f6
Derek Pyle <derekpyle@gmail.com>
2020-04-11 15:03:29 -0400

    Checking typos for errors, post build rejected #2

--- index.js
+++ index.js
@@ -25,17 +25,17 @@ require('./routes/billingRoutes')(app);
 
 mongoose.connect(keys.mongoURI);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   //making sure express will serve up production assets
   //like main.js and main.css
-  app.use(express.static('client/build'))
+  app.use(express.static("client/build"));
 
   //Express will serve index.html if it doesnt
   //recognize the route
-  const path = require('path')
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  const path = require("path");
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
